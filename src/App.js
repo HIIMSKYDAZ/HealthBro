51036fda4b4f727a8e03a9c27e96a3562602e46d
LudvigP <75888209+HIIMSKYDAZ@users.noreply.github.com>
2024-11-12 05:11:43 -1100

    Projekt 2024.11.12
    menu on going

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Router>
         <NavBar />
-        <Routes>
-          <Route path="/" exact ></Route>
-        </Routes>
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
       </Router>
     </>
   );
