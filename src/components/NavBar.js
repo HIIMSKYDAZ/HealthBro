29e8040bb5984e66f3ca84f80b9d4952ba60933a
HIIMSKYDAZ <75888209+HIIMSKYDAZ@users.noreply.github.com>
2024-11-16 12:56:06 +0100

    merge

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button'
 import './NavBar.css'
+import { faDisplay } from '@fortawesome/free-solid-svg-icons'
 
 function NavBar() {
   const [click, setClick] = useState(false)
