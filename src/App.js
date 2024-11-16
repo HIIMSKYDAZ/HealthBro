597182c25e7df2f14ae5338d1c8ea10a3fe28644
SzilagyiJ <112682509+szilagyij@users.noreply.github.com>
2024-11-16 14:19:29 +0100

    Contact page létrehozva

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import { BrowserRouter as Router, Switch, Routes, Route } from "react-router-dom";
 import AboutPage from "./AboutPage.jsx";
+import ContactPage from "./ContactPage.jsx";
 
 
 //geci
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<h1>Home Page</h1>} /> {/* Ideiglenes kezdőoldal */}
           <Route path="/about" exact element={<AboutPage />} />
+          <Route path="/contact" exact element={<ContactPage />}></Route>
         </Routes>
       </Router>
     </>
