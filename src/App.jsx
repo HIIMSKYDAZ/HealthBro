a03123838b5fee30682f4bce36d157ef806165c5
HIIMSKYDAZ <75888209+HIIMSKYDAZ@users.noreply.github.com>
2024-11-16 14:18:00 +0100

    Home stable
    Cardokon kivül működik minden

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import NavBar from "./components/NavBar.js";
+import { BrowserRouter as Router, Switch, Routes, Route } from "react-router-dom";
+import AboutPage from "./AboutPage.jsx";
+import Home from "./components/pages/Home";
+
+
+//geci
+function App() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" exact Component={Home}/> {/* Ideiglenes kezdőoldal */}
+          <Route path="/about" exact element={<AboutPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import '../../App.css';
+import HeroSectionfrom from '../HeroSection';
+
+function Home() {
+    return (
+        <>
+            <HeroSectionfrom />
+        </>
+    );
+}
+
+export default Home
