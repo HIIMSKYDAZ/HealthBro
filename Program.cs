a91e5376d11e5b9aca54a5abdb9788d7b43c8db4
pocsaig <112682762+pocsaig@users.noreply.github.com>
2024-12-03 10:51:04 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using HealthBroAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseMySQL("server=localhost;database=healthbro;user=root;password="));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
