7b4ad05417058d2127809e3a2ccfdbea8697a645
HIIMSKYDAZ <75888209+HIIMSKYDAZ@users.noreply.github.com>
2024-12-15 11:54:16 +0100

    loginpage

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { React } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./SignupPages.css";
 import { Button } from '../Button';
@@ -7,34 +8,42 @@ function LoginPage() {
     return (
       <>
         <video src="../videos/herobg.mp4" autoPlay loop muted />
+<div>
         <div className="mask d-flex align-items-center justify-content-center container-fluid mx-auto mt-3 mb-3">
           <div className="container">
             <div className="row d-flex justify-content-center align-items-center">
               <div className="col-12 col-md-6 col-lg-5 col-xl-4">
                 <div className="card" style={{ borderRadius: "15px" }}>
                   <div className="card-body p-3">
-
+<h2 className="text-uppercase text-center mb-4">
+                                        Belépés
+                                    </h2>
   
                     <form>
-                      <div data-mdb-input-init className="form-outline mb-3">
+                      <div className="form-outline mb-4">
                         <input
-                          type="text"
-                          id="form3Example1cg"
+                          type="email"
+                          id="form3Example3cg"
                           className="form-control form-control-sm"
                         />
-                        <label className="form-label" htmlFor="form3Example1cg">
-                          Felhasználónév
+                        <label
+className="form-label"    
+htmlFor="form3Example3cg"
+                                            >
+                                                Email
                         </label>
                       </div>
 
-  
-                      <div data-mdb-input-init className="form-outline mb-3">
+                        <div className="form-outline mb-4">
                         <input
                           type="password"
                           id="form3Example4cg"
                           className="form-control form-control-sm"
                         />
-                        <label className="form-label" htmlFor="form3Example4cg">
+                        <label
+className="form-label"
+htmlFor="form3Example4cg"
+>
                           Jelszó
                         </label>
                       </div>
@@ -43,17 +52,19 @@ function LoginPage() {
                       <Button
                       buttonStyle="btn--primary"
                       buttonSize="btn--large"
-                      linkTo="/HomeMain"
-                      >
-                      Bejelentkezés
+                                            >
+                      Belépés
                     </Button>
-
-
                     </div>
 
-
-                      
+<p className="text-center text-muted mt-5 mb-0">
+                                            Nincs Fiókod?
+                                            <Link to="/sign-up" className="fw-bold text-body">
+                                                Regisztráció
+                                            </Link>
+                                        </p>                      
                     </form>
+</div>
                   </div>
                 </div>
               </div>
