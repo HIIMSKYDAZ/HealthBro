54a96a740b29f6408c2b1acadc1b2b8ea0582580
pocsaig <112682762+pocsaig@users.noreply.github.com>
2024-12-15 19:31:00 +0100

    Add files via upload

--- healthbro.sql
+++ healthbro.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Dec 03. 08:58
+-- Létrehozás ideje: 2024. Dec 13. 07:56
 -- Kiszolgáló verziója: 10.4.32-MariaDB
 -- PHP verzió: 8.2.12
 
@@ -39,6 +39,27 @@ CREATE TABLE `exercises` (
 -- --------------------------------------------------------
 
 --
+-- Tábla szerkezet ehhez a táblához `permission`
+--
+
+CREATE TABLE `permission` (
+  `Id` int(11) NOT NULL,
+  `Level` int(1) NOT NULL,
+  `Name` varchar(32) NOT NULL,
+  `Description` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `permission`
+--
+
+INSERT INTO `permission` (`Id`, `Level`, `Name`, `Description`) VALUES
+(1, 0, 'Luzer', 'Webes regisztráció felhasználó'),
+(2, 9, 'Administrator', 'Rendszergazda');
+
+-- --------------------------------------------------------
+
+--
 -- Tábla szerkezet ehhez a táblához `planexercises`
 --
 
@@ -53,16 +74,27 @@ CREATE TABLE `planexercises` (
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `users`
+-- Tábla szerkezet ehhez a táblához `user`
 --
 
-CREATE TABLE `users` (
-  `UserID` int(11) NOT NULL,
-  `UserName` varchar(50) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `PasswordHash` varchar(255) NOT NULL,
-  `RegistrationDate` datetime DEFAULT current_timestamp()
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL,
+  `LoginNev` varchar(16) NOT NULL,
+  `HASH` varchar(64) NOT NULL,
+  `SALT` varchar(64) NOT NULL,
+  `Name` varchar(64) NOT NULL,
+  `PermissionId` int(11) NOT NULL,
+  `Active` tinyint(1) NOT NULL,
+  `Email` varchar(64) NOT NULL,
+  `ProfilePicturePath` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `user`
+--
+
+INSERT INTO `user` (`Id`, `LoginNev`, `HASH`, `SALT`, `Name`, `PermissionId`, `Active`, `Email`, `ProfilePicturePath`) VALUES
+(1, 'kerenyir', 'd5fe0e517520122f1ab363b6b7ee9ae616e7ad393693ef00d81a7f287a79931a', 'Gm63C4jiWnYvfZfiKUu2cu8AHPNDj8NoHhtQn88yiJhyOunBNSd7tRoWo5wwqg9X', 'Kerényi Róbert', 2, 1, 'kerenyir@kkszki.hu', 'img\\kerenyir.jpg');
 
 -- --------------------------------------------------------
 
@@ -115,6 +147,14 @@ ALTER TABLE `exercises`
   ADD PRIMARY KEY (`ExerciseID`);
 
 --
+-- A tábla indexei `permission`
+--
+ALTER TABLE `permission`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Szint` (`Level`),
+  ADD UNIQUE KEY `Nev` (`Name`);
+
+--
 -- A tábla indexei `planexercises`
 --
 ALTER TABLE `planexercises`
@@ -123,11 +163,13 @@ ALTER TABLE `planexercises`
   ADD KEY `ExerciseID` (`ExerciseID`);
 
 --
--- A tábla indexei `users`
+-- A tábla indexei `user`
 --
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`UserID`),
-  ADD UNIQUE KEY `Email` (`Email`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `LoginNev` (`LoginNev`),
+  ADD UNIQUE KEY `Email` (`Email`),
+  ADD KEY `Jog` (`PermissionId`);
 
 --
 -- A tábla indexei `workoutdetails`
@@ -162,16 +204,22 @@ ALTER TABLE `exercises`
   MODIFY `ExerciseID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT a táblához `permission`
+--
+ALTER TABLE `permission`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT a táblához `planexercises`
 --
 ALTER TABLE `planexercises`
   MODIFY `PlanExerciseID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT a táblához `users`
+-- AUTO_INCREMENT a táblához `user`
 --
-ALTER TABLE `users`
-  MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT a táblához `workoutdetails`
@@ -203,6 +251,12 @@ ALTER TABLE `planexercises`
   ADD CONSTRAINT `planexercises_ibfk_2` FOREIGN KEY (`ExerciseID`) REFERENCES `exercises` (`ExerciseID`);
 
 --
+-- Megkötések a táblához `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`PermissionId`) REFERENCES `permission` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
 -- Megkötések a táblához `workoutdetails`
 --
 ALTER TABLE `workoutdetails`
@@ -213,13 +267,13 @@ ALTER TABLE `workoutdetails`
 -- Megkötések a táblához `workoutplans`
 --
 ALTER TABLE `workoutplans`
-  ADD CONSTRAINT `workoutplans_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
+  ADD CONSTRAINT `workoutplans_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Megkötések a táblához `workouts`
 --
 ALTER TABLE `workouts`
-  ADD CONSTRAINT `workouts_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
+  ADD CONSTRAINT `workouts_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `user` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
