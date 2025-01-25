8751a5e2652e1d82ad61ac29e242110343536167
LudvigP <75888209+HIIMSKYDAZ@users.noreply.github.com>
2025-01-24 20:54:37 -1100

    workoutplan

--- src/components/WorkoutCards.jsx
+++ src/components/WorkoutCards.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React from 'react';
+import './WorkoutCards.css';
 
-const WorkoutCards=()=>{
-    
-}
+const WorkoutCards = ({ imageSrc, cardName }) => {
+    return (
+      <div className="card">
+        <img src={imageSrc} alt={cardName} style={{ width: "100%", height: "auto" }} />
+        <div style={{ padding: "8px", backgroundColor: "#f4f4f4" }}>{cardName}</div>
+      </div>
+    );
+  };
+
+export default WorkoutCards;
--- src/components/pages/WorkoutPage.jsx
+++ src/components/pages/WorkoutPage.jsx
@@ -3,6 +3,8 @@ import Sidebar from "../SideBar";
 import "./HomeMain.css";
 import { Button } from '../Button'; 
 import { useNavigate } from "react-router-dom";
+import WorkoutCards from"../WorkoutCards.jsx";
+
 
 export const WorkoutPage = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -22,6 +24,14 @@ export const WorkoutPage = () => {
                 <div style={{position: "absolute", top: 0, right: 0}}>
                     <Button linkTo={"/"}>Új terv létrehozása</Button>
                 </div>
+                <h1>Workout</h1>
+                <div className="row-hb">
+  {Array(6).fill(0).map((_, index) => (
+    <a href="/workoutpage" key={index}>
+      <WorkoutCards imageSrc="../images/user1.jpg" cardName={`Workout ${index + 1}`} />
+    </a>
+  ))}
+</div>
             </div>
         </div>
     );
