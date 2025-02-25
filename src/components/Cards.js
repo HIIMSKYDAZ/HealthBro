58a4f61533f403c2209adc857dbad7e79ed6ddd5
SzilagyiJ <112682509+szilagyij@users.noreply.github.com>
2025-02-25 10:22:33 +0100

    review fix

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,53 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import CardItem from './CardItem.js'
 import './Cards.css'
 
 function Cards() {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+      axios
+        .get("https://localhost:5000/api/Review")
+        .then((response) => {
+          setReviews(response.data);
+        })
+        .catch((error) => {
+          console.error("Hiba az adatok lekérésekor:", error);
+        });
+    }, []);
   return (
     <div className='cards'>
         <h1 style={{color: "black"}}>Vásárlók</h1>
         <div className='cards__container'>
             <div className='cards__wrapper'>
                 <ul className='cards__items'>
-                    <CardItem
-                        src='images/user1.jpg'
-                        text='Nagyon elégedett voltam!'
-                        label='Vélemény'
-                        path='/services'
-                    />
-                    <CardItem
-                        src='images/user2.jpg'
-                        text='A legjobb fitness app!'
-                        label='Vélemény'
-                        path='/services'
-                    />
-                    <CardItem
-                        src='images/user3.jpg'
-                        text='Megváltoztatta az életem!'
-                        label='Vélemény'
-                        path='/services'
-                    />
-                </ul>
-                <ul className='cards__items'>
-                    <CardItem
-                        src='images/user1.jpg'
-                        text='Nagyon elégedett voltam!'
-                        label='Vélemény'
-                        path='/services'
-                    />
-                    <CardItem
-                        src='images/user2.jpg'
-                        text='A legjobb fitness app!'
-                        label='Vélemény'
-                        path='/services'
-                    />
-                    <CardItem
-                        src='images/user3.jpg'
-                        text='Megváltoztatta az életem!'
-                        label='Vélemény'
-                        path='/services'
-                    />
-                    
+                {reviews.map((review, index) => (
+              <CardItem
+                key={index}
+                src={`http://healthbro.nhely.hu/users/${review.profilePicturePath}`}
+                text={review.velemeny}
+                label='Vélemény'
+                path="/services"
+              />
+            ))}
                 </ul>
             </div>
         </div>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -9,7 +9,6 @@ function Home() {
     <>
       <HeroSectionfrom />
       <Cards />
-      <Reviews />
     </>
   );
 }
