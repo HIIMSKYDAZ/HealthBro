7d972537fb9fca70a6d59a13c1b67b498f83919c
pocsaig <112682762+pocsaig@users.noreply.github.com>
2025-03-31 10:02:20 +0200

    Update LoggedUsercs.cs

--- DTOs/LoggedUsercs.cs
+++ DTOs/LoggedUsercs.cs
@@ -2,11 +2,11 @@
 {
     public class LoggedUsercs
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
         public int? Permission { get; set; }
-        public string ProfilePicturePath { get; set; }
+        public string? ProfilePicturePath { get; set; }
 
-        public string Token { get; set; }
+        public string? Token { get; set; }
     }
 }
