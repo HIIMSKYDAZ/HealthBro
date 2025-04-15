72d9c7e6805e3d3470f8489c293c6867e5dfd05b
LudvigP <75888209+HIIMSKYDAZ@users.noreply.github.com>
2025-04-15 11:30:33 +0200

    fix

--- src/components/pages/WorkoutPage.jsx
+++ src/components/pages/WorkoutPage.jsx
@@ -96,7 +96,7 @@ export const WorkoutPage = () => {
                         workouts.map((workout, index) => (
                             <a className="a-hb" href={`/WorkoutPlanSingle/${workout.planId}`} key={index}>
                                 <WorkoutCards 
-                                    imageSrc={workout.imageUrl || "http://healthbro.nhely.hu/default.jpg"} 
+                                    imageSrc={"http://healthbro.nhely.hu/default.jpg"} 
                                     cardName={workout.planName}
                                     planId={workout.planId}
                                     onDelete={handleDeleteWorkout}
